d7d61e0e079f0384dde15393a3717770e4b00d1c
Yawar Ali <yawar_ali34@hotmail.com>
2018-09-24 19:19:46 +0500

    Added Identity Related Pages and Classes

---
+++ Jamia/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Jamia.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Jamia.Areas.Identity.IdentityHostingStartup))]
+namespace Jamia.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
