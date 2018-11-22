cb07fd8758f7d56a3907ccfc22af4ebdf764d929
Yawar Ali <yawar.ali.pucit@gmail.com>
2018-11-22 19:30:40 +0500

    DB Name Changed

--- Jamia/Infrastructure/Names.cs
+++ Jamia/Infrastructure/Names.cs
@@ -2,21 +2,27 @@
 {
     public class AreaNames
     {
-        public static string Admin = "Admin";
-        public static string Student = "Student";
-        public static string Teacher = "Teacher";
+        public const string Admin = "Admin";
+        public const string Student = "Student";
+        public const string Teacher = "Teacher";
     }
     public class ControlerNames
     {
-        public static string Home = "Home";
+        public const string Home = "Home";
 
         #region Admin
-        public static string Sessions = "Sessions"; 
-        public static string Courses = "Courses"; 
+        public const string Sessions = "Sessions"; 
+        public const string Courses = "Courses"; 
         #endregion
     }
     public class ActionNames
     {
-        public static string Index = "Index";
+        public const string Index = "Index";
+    }
+    public class SchemaNames
+    {
+        public const string Security = "Security";
+        public const string Admin = "Admin";
+        public const string Student = "Student";
     }
 }
